0c729ed1dc29e8ac06e7b17127bd79ff734ab53b
BParCab <baparedes89@gmail.com>
2026-02-28 19:20:15 -0600

    updated

--- app/src/main/java/com/example/theperfectionist/MainActivity.kt
+++ app/src/main/java/com/example/theperfectionist/MainActivity.kt
@@ -22,56 +22,11 @@ class MainActivity : ComponentActivity() {
 
     lateinit var btManager: BluetoothManager
 
-    /*@RequiresApi(Build.VERSION_CODES.S)
-    private val btPermissionsLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { perms ->
-            val connectGranted = perms[android.Manifest.permission.BLUETOOTH_CONNECT] == true
-            val scanGranted = perms[android.Manifest.permission.BLUETOOTH_SCAN] == true
-            Log.d("BT", "CONNECT=$connectGranted, SCAN=$scanGranted")
-        }
-
-    private val locationPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { perms ->
-            val fineGranted = perms[android.Manifest.permission.ACCESS_FINE_LOCATION] == true
-            val coarseGranted = perms[android.Manifest.permission.ACCESS_COARSE_LOCATION] == true
-            Log.d("LOC", "FINE=$fineGranted, COARSE=$coarseGranted")
-        }
-
-
-    private val enableBluetoothLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                Log.d("BT", "Bluetooth enabled")
-            }
-        }*/
-
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         btManager = BluetoothManager(this)
-
-        // Request BOTH permissions
-        /*btPermissionsLauncher.launch(
-            arrayOf(
-                android.Manifest.permission.BLUETOOTH_CONNECT,
-                android.Manifest.permission.BLUETOOTH_SCAN
-            )
-        )
-        // Request both permissions
-        locationPermissionLauncher.launch(
-            arrayOf(
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            )
-        )
-
-        // Optional: enable Bluetooth if off
-        val systemAdapter = android.bluetooth.BluetoothAdapter.getDefaultAdapter()
-        if (systemAdapter != null && !systemAdapter.isEnabled) {
-            val intent = Intent(android.bluetooth.BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            enableBluetoothLauncher.launch(intent)
-        }*/
 
         setContent {
             val navController = rememberNavController()
